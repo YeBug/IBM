d98ec38d727d13a485bc3eff343b3f102fb4fdea
YeBug <donald_hunter_c@163.com>
2019-06-07 00:44:51 +0800

    Update E3.py
    添加12项产品关系图绘制

--- Cross-Shell_Streams/E3.py
+++ Cross-Shell_Streams/E3.py
@@ -1,7 +1,10 @@
 from tools import constant as constant, utils as utils
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 
 Count = [[0] * 12 for i in range(12)]
+Coordinate = [[0] * 2 for i in range(12)]
 
 
 def build_count_list(data):
@@ -15,11 +18,41 @@ def build_count_list(data):
     return 0
 
 
+def circle_xy(r=20, side_num=12):
+    theta = np.linspace(0, 2*np.pi, side_num, False)
+    x = r*np.sin(theta)
+    x = np.append(x, x[0])
+    y = r*np.cos(theta)
+    y = np.append(y, y[0])
+    return x, y
+
+
+def draw_graph(x, y, count, value):
+    product = 1
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set(title="Relationship", aspect='equal')
+    for i in range(1, 12):
+        for j in range(i+1, 13):
+            c_x = [x[i-1], x[j-1]]
+            c_y = [y[i-1], y[j-1]]
+            if count[i-1][j-1] > value*2:
+                plt.plot(c_x, c_y, lw=3, color='black')
+            elif count[i-1][j-1] > value:
+                plt.plot(c_x, c_y, lw=1, color='black')
+    x = np.delete(x, 12, 0)
+    y = np.delete(y, 12, 0)
+    for i, j in zip(x, y):
+        plt.text(i, j, "product" + str(product), ha='center', va='bottom', fontsize=10)
+        product += 1
+
+
 if __name__ == '__main__':
     print('start')
     data = utils.read_file(os.path.join(constant.data_files_path, constant.cust_prod_dat_name))
     build_count_list(data)
-    for i in range(12):
-        print(Count[i])
-        print('\n')
+    x, y = circle_xy()
+    draw_graph(x, y, Count, 4482)
+
+    plt.show()
     print("end")
